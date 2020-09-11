6cf657ea5d2203754fb7e363e899821e0255c094
dhayaalan <dhayaalanjayakumar2001@gmail.com>
2020-09-11 17:55:50 +0530

    test

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
-const http = require("http");
-
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middlewares");
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("In the another middleware");
-  res.send("<h1>Hello from express</h1>");
-});                
-
-app.listen(2000);
+app.listen(3000);
